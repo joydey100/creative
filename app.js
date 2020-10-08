3955c91b8cd70834c7b875b3aa6f0a46863f8392
joydey100 <68456193+joydey100@users.noreply.github.com>
2020-10-08 18:58:29 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+function menuToggle(){
+    const toggle = document.querySelector('.toggle');
+    const navigation = document.querySelector('.navigation');
+    toggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
